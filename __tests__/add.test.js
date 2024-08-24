879a6771dae645ac0c793265ee07d409edc50981
kola kolade <kolakolade@gmail.com>
2024-08-24 17:58:23 +0100

    Working

--- __tests__/add.test.js
+++ __tests__/add.test.js
@@ -1,8 +1,8 @@
-import add from '@api/lib/add.js';
 import { describe, expect, it } from 'vitest';
+import add from '../api/v1/lib/add.js';
 
 describe('Add function', () => {
   it('adds 1 + 2 and to equal 3', () => {
-    expect(add(1, 2).toBe(3));
+    expect(add(1, 2)).toBe(3);
   });
 });
